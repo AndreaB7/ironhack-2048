fe8b483eebbb65b996ab4d0d505c780623c9d635
Tair Assimov <tair@assimov.net>
2017-06-21 11:48:44 +0200

    Add tile generation and start with two of them
    The tile generation method randomly picks either 2 or 4, where
    2 has 80% chance to appear and 4 only 20%. Then the method tries
    to find an empty slot on the board and if it exists, adds the generated
    tile to the board.
    
    On game initialization, we generate two tiles.

--- js/game.js
+++ js/game.js
@@ -13,7 +13,23 @@ function Game2048() {
   // Track the game status, whether it was won or lost
   this.won   = false;
   this.lost  = false;
+
+  // Start with two tiles
+  this._generateTile();
+  this._generateTile();
 }
+
+// Generates a tile of 2 or 4 and places it on the board.
+Game2048.prototype._generateTile = function() {
+  // Set initial value to either 2 with 80%, or 4 with 20%
+  var initialValue = (Math.random() < 0.8) ? 2 : 4;
+  var emptyTile = this._getAvailablePosition();
+
+  // If empty tile is found, set it on the board
+  if (emptyTile) {
+    this.board[emptyTile.x][emptyTile.y] = initialValue;
+  }
+};
 
 // Finds empty position on the board
 Game2048.prototype._getAvailablePosition = function() {
