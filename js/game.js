2eda88156c8e069c66044252ea21b500bd42441b
Tair Assimov <tair@assimov.net>
2017-06-21 11:39:41 +0200

    Add method to get available position on the board
    To start off, we'd need to randomly place two tiles on the board.
    However, we don't know which slots on the board are available.
    Hence, this method finds an empty slot on the board.
    
    If its found, it will return its coordinates.
    If not (board is full), it will return false.

--- js/game.js
+++ js/game.js
@@ -14,3 +14,25 @@ function Game2048() {
   this.won   = false;
   this.lost  = false;
 }
+
+// Finds empty position on the board
+Game2048.prototype._getAvailablePosition = function() {
+  // Store all emoty slots on the board
+  var emptyTiles = [];
+
+  // Iterate over board array and if the slot is empty
+  // store the coordinates of this position in emptyTiles
+  this.board.forEach(function(row, rowIndex) {
+    row.forEach(function(elem, colIndex) {
+      if (!elem) emptyTiles.push({ x: rowIndex, y: colIndex });
+    });
+  });
+
+  // If it's full, return false
+  if (emptyTiles.length === 0)
+    return false;
+
+  // Otherwise, get the random position and return its coordinates
+  var randomPosition = Math.floor(Math.random() * emptyTiles.length);
+  return emptyTiles[randomPosition];
+};
